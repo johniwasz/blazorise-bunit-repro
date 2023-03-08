7aa3c4410e3cb1dbec4da188840ca8aca202647d
John Iwasz <iwaszj@gmail.com>
2023-03-07 21:29:32 -0500

    Commiting repro test.

---
+++ BlazorTestProject1/CounterTests.cs
@@ -0,0 +1,40 @@
+﻿using Blazorise.Bootstrap;
+using Blazorise;
+using BlazorWebApp.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blazorise.Icons.FontAwesome;
+
+namespace BlazorTestProject1
+{
+    public class CounterTests : TestContext
+    {
+        [Fact]
+        public void CounterStartsAtZero()
+        {
+            // To make this test pass.
+            //  1. Comment out the Services block
+            //  2. Comment out the Blazorize Button in the Counter Component
+            
+            Services
+                .AddBlazorise(options =>
+                {
+                    options.Immediate = true;
+                })
+                .AddBootstrapProviders()
+                .AddFontAwesomeIcons();
+
+            
+            // Arrange
+            var cut = RenderComponent<Counter>();
+
+            // Assert that content of the paragraph shows counter at zero
+           cut.Find("p").MarkupMatches("<p role=\"status\">Current count: 0</p>");
+        }
+      
+
+    }
+}
---
+++ BlazorWebApp/Components/Counter.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorWebApp.Components
+{
+    public partial class Counter
+    {
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
